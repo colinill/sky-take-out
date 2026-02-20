44ee92c7e0db257b9e7d140682af3bc8d586f1a1
colinill <2802054664@qq.com>
2026-02-20 15:25:57 +0800

    添加 定时处理订单状态/来单提醒/用户催单 功能

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,59 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 定时任务类,用于处理订单状态
+ */
+@Component
+@Slf4j
+public class OrderTask {
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 处理超时订单
+     */
+    @Scheduled(cron = "0 * * * * ? ")//每分钟触发一次
+    public void processTimeoutOrder(){
+        log.info("处理超时订单:{}", LocalDateTime.now());
+
+        //查询15分钟之前的待支付订单
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+        List<Orders> list = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, time);
+
+        if(list!=null&& !list.isEmpty()){
+            for (Orders orders : list) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+
+    /**
+     * 处理一直派送中的订单
+     */
+    @Scheduled(cron = "0 0 1 * * ?")//每天凌晨一点
+    public void processDeliveryOrder(){
+        log.info("处理一直派送中的订单:{}",LocalDateTime.now());
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
+        List<Orders> list = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+
+        if(list!=null&& !list.isEmpty()){
+            for (Orders orders : list) {
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
